d74b26b14e758f5b791f341127ba17606979e055
Adrian Stainforth <adrian@morsedigital.com>
2016-06-24 16:05:33 +0100

    fixing status issue

--- src/check_status.js
+++ src/check_status.js
@@ -5,7 +5,7 @@ let success_status = [200, 201];
 let error_status = [404, 500];
 
 module.exports = function(status, readyState){
-  if (!_.includes(success_status, 200) && readyState === 4) return -1;
+  if (!_.includes(success_status, status) && readyState === 4) return -1;
   if (readyState === 4) return 1;
   return 0;
 };
